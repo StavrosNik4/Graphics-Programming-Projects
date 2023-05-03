983d5fc85727e2ba6f7e7b1e7b4e3c1c3a0ac09b
Stavros Nikolaidis <stavniko@csd.auth.gr>
2023-05-03 21:45:33 +0300

    Update project2.c

--- project2.c
+++ project2.c
@@ -1,13 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <GL/glut.h>
 #include <stdbool.h>
 
-float scaleFactor = 1.0f;    // current scaling factor
+float scaleFactor = 1.0f;      // current scaling factor
 float scaleSpeed = 0.0005f;   // speed at which to scale the cube
 bool increasing = true;      // whether to increase or decrease the scale factor
 
 float rotate_angle = 0.0f; // initial rotation angle
 const float rotate_axis[] = { 1.0f, 1.0f, 1.0f }; // rotation axis
 const int square = 1;
+
+#define PI 3.1415926535898
+
+float angle = 0.0f;
+float radius = 50.0f;
+float objectX = 0.0f;
+float objectY = 0.0f;
+
 
 void myinit(void) {
     glEnable(GL_BLEND);
@@ -30,8 +41,6 @@ void myinit(void) {
 
 void drawCube() {
     glPushMatrix();
-
-    //glScalef(4.0f, 4.0f, 4.0f); // scale the cube to be of size 4
 
     glTranslatef(0.0f, 0.0f, -0.5f);// Apply translation to move the cube's center to the origin
 
@@ -82,7 +91,6 @@ void drawCube() {
 
 }
 
-
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -91,32 +99,43 @@ void display() {
 
     // set up camera
     gluLookAt(0.0f, 0.0f, 0.0f,  // camera position
-        0.0f, 0.0f, -1.0f, // point to look at
-        0.0f, 1.0f, 0.0f); // up vector
+              0.0f, 0.0f, -1.0f, // point to look at
+              0.0f, 1.0f, 0.0f); // up vector
+
+    // Move the object along the circle
+    objectX = radius * cos(angle);
+    objectY = radius * sin(angle);
+
+    // Draw the circle
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 360; i++)
+    {
+        float theta = i * PI / 180.0f;
+        glVertex2f(radius * cos(theta), radius * sin(theta));
+    }
+    glEnd();
+
+    glPushMatrix();
+    glTranslatef(objectX, objectY, -80.0f);
+    
+    // rotate the cube
+    glRotatef(rotate_angle, rotate_axis[0], rotate_axis[1], rotate_axis[2]); // Apply rotation transformations
 
     // scale the cube
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, -100.0f);
-
-    // rotate the cube
-   // glRotatef(rotate_angle, rotate_axis[0], rotate_axis[1], rotate_axis[2]);
-
-    glRotatef(rotate_angle, 1.0f, 1.0f, 1.0f);// Apply rotation transformations
     glScalef(4.0 * scaleFactor, 4.0 * scaleFactor, 4.0 * scaleFactor);
-    drawCube();  // call your function to draw the cube
 
+    // Draw the object
+    drawCube();  // call your function to draw the cube
     glPopMatrix();
 
     glutSwapBuffers();// swap front and back buffers
-    //glFlush(); /* clear buffers */
-
 }
 
 void reshape(int w, int h) {
     glViewport(0, 0, (GLsizei)w, (GLsizei)h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-w / 20, w / 20, -h / 20, h / 20, -10.0, 200.0);
+    glOrtho(-100.0, 100.0, -100.0, 100.0, 0.01, 300.0);
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -142,6 +161,13 @@ void idle() {
     if (rotate_angle >= 360.0f) {
         // if rotation completes a full circle, start growing again
         rotate_angle = 0.0f;
+    }
+
+    // Increase the angle to move the object
+    angle += 0.0003f;
+    if (angle >= 360.0f)
+    {
+        angle = 0.0f;
     }
 
     glutPostRedisplay();
